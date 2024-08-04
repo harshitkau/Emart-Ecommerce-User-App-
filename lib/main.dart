07f263a651ee4f3a8bad29118169c3b59fcd1608
harshitkau <88334621+harshitkau@users.noreply.github.com>
2024-08-04 11:45:12 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,8 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+
+
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: appname,
